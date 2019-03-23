487ba07ca6cdcdeaa2e01b526b1e4dbe07f26928
Vigneshwaran Arumugam <varumugam@ooyala.com>
2019-03-23 14:20:35 +0530

    App components were added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AddTodoComponent } from './modules/components/add-todo/add-todo.component';
+import { UpdateTodoComponent } from './modules/components/update-todo/update-todo.component';
+import { CompletedTodoComponent } from './modules/components/completed-todo/completed-todo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddTodoComponent,
+    UpdateTodoComponent,
+    CompletedTodoComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/modules/components/completed-todo/completed-todo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-completed-todo',
+  templateUrl: './completed-todo.component.html',
+  styleUrls: ['./completed-todo.component.css']
+})
+export class CompletedTodoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
